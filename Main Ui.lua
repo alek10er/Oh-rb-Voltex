fd0e3e3759f333701decdbf11ba4d8c7c7cc6c1b
alek10er <157598100+alek10er@users.noreply.github.com>
2025-10-26 00:05:16 +0300

    Update Main Ui.lua

--- Main Ui.lua
+++ Main Ui.lua
@@ -62,7 +62,7 @@ local themeStyles = {
 	-- Цвет фона в правой части UI
 	Background = Color3.fromRGB(15,15,15),
 	-- Цвет фона в левой части UI
-    Header = Color3.fromRGB(15,15,15),
+    Header = Color3.fromRGB(31,31,31),
 	-- Цвет текста
     TextColor = Color3.fromRGB(255,255,255),
 	-- Цвет фона у кнопок
